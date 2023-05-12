9e86910db9eb837619459ae8d1555c02792b814e
marcoantoniocasanova <117048491+marcoantoniocasanova@users.noreply.github.com>
2023-05-12 13:21:43 -0400

    cambios para activar edición en educacion y acercademi

--- src/app/componentes/acercademi/acercademi.component.ts
+++ src/app/componentes/acercademi/acercademi.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'acercademi-component',
   templateUrl: './acercademi.component.html',
   styleUrls: ['./acercademi.component.scss']
 })
-export class AcercademiComponent { }
+export class AcercademiComponent implements OnInit {
+  displayElement = true;
+  titulo: string = "";
+  texto: string = "";
+
+  constructor() { }
+  ngOnInit() {
+    this.titulo = "Hola! Marco Casanova"
+    this.texto = `¡Es un gusto tenerte por aquí! Mi nombre es Marco Antonio Casanova. Desde pequeño me
+    encantan los videojuegos. Por esta razón, siempre me ha interesado saber
+    lo que pasa tras los bastidores del mundo de la programación`;
+  }
+
+  updateEditMode() {
+    this.displayElement = !this.displayElement;
+  }
+
+  updateAcercademiTitle(event: any){
+
+    this.titulo = event.target.value
+
+  }
+
+
+  updateAcercademiTexto(event: any) {
+    this.texto  = event.target.value
+  }
+}
