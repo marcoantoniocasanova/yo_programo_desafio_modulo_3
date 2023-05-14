7d6370ef9b5c97d4b53316ebdfda45c659ee9e31
marcoantoniocasanova <117048491+marcoantoniocasanova@users.noreply.github.com>
2023-05-14 18:54:33 -0400

    cambios para visualizar logos en educación y experiencia

--- src/app/componentes/educacion/educacion.component.ts
+++ src/app/componentes/educacion/educacion.component.ts
@@ -14,18 +14,18 @@ educacion: EduTyp[] = [];
 constructor() { }
 ngOnInit() {
   this.educacion = [
-    { id: 0, fecha: "1988", url: "https://www.ucab.edu.ve", urlTitle: "", img: "images/UCAB.png", text: "Licenciatura en Comunicación Social // Universidad Católica Andrés Bello." },
-    { id: 1, fecha: "2005", url: "https://www.instagram.com/upel_caracas/?hl=es", urlTitle: "", img: "images/UPEL.png", text: "Componente Docente // Universidad Pedagógica Experimental Libertador." },
-    { id: 2, fecha: "2010", url: "https://www.ujmv.edu/", urlTitle: "", img: "/images/UJMV.png", text: "Programa de Formación de Educadores a Distancia // Universidad José María Vargas." },
-    { id: 3, fecha: "2011", url: "https://www.sena.edu.co/es-co/Paginas/default.aspx", urlTitle: "", img: "images/SENA.jpeg", text: "Programación con HTML y Javascript // Servicio Nacional de Aprendizaje (SENA)." },
-    { id: 4, fecha: "2011", url: "https://www.sena.edu.co/es-co/Paginas/default.aspx", urlTitle: "", img: "images/SENA.jpeg", text: "Planeación Estratégica de Proyectos // Servicio Nacional de Aprendizaje (SENA)." },
-    { id: 5, fecha: "2011", url: "https://www.sena.edu.co/es-co/Paginas/default.aspx", urlTitle: "", img: "images/SENA.jpeg", text: "Diseño de Instrumentos de Evaluación // Servicio Nacional de Aprendizaje (SENA)." },
-    { id: 6, fecha: "2012 - 2013", url: "https://www.sena.edu.co/es-co/Paginas/default.aspx", urlTitle: "", img: "images/SENA.jpeg", text: "Inglés (Curso completo) Niveles I al V // Servicio Nacional de Aprendizaje (SENA).." },
-    { id: 7, fecha: "2015", url: "https://learndigital.withgoogle.com/activate/courses", urlTitle: "", img: "images/GOOGLE-ACT.png", text: "Introducción al Desarrollo Web // Google Activate." },
-    { id: 8, fecha: "2015", url: "https://www.msjc.edu/", urlTitle: "", img: "images/MSJC.png", text: "Fundamental English Writing // Mt. San Jacinto College." },
-    { id: 9, fecha: "2017", url: "https://iei.ua.es/", urlTitle: "", img: "images/IEI-ALICANTE.jpg", text: "Curso de Desarrollo Web // Instituto de Economía Internacional de la Universidad de Alicante." },
-    { id: 10, fecha: "2022 - 2022", url: "https://yoprogramo.org.ar/modulos-fase-1/", urlTitle: "", img: "images/SeProg.png", text: "#SéProgramar // Argentina Programa , INTI." },
-    { id: 11, fecha: "2022 - Actual", url: "https://www.yoprogramo.org.ar/", urlTitle: "", img: "images/YoProg.png", text: "#YoProgramo // Argentina Programa , INTI." },
+    { id: 0, fecha: "1988", url: "https://www.ucab.edu.ve", urlTitle: "", img: "UCAB.png", text: "Licenciatura en Comunicación Social // Universidad Católica Andrés Bello." },
+    { id: 1, fecha: "2005", url: "https://www.instagram.com/upel_caracas/?hl=es", urlTitle: "", img: "UPEL.png", text: "Componente Docente // Universidad Pedagógica Experimental Libertador." },
+    { id: 2, fecha: "2010", url: "https://www.ujmv.edu/", urlTitle: "", img: "UJMV.png", text: "Programa de Formación de Educadores a Distancia // Universidad José María Vargas." },
+    { id: 3, fecha: "2011", url: "https://www.sena.edu.co/es-co/Paginas/default.aspx", urlTitle: "", img: "SENA.jpeg", text: "Programación con HTML y Javascript // Servicio Nacional de Aprendizaje (SENA)." },
+    { id: 4, fecha: "2011", url: "https://www.sena.edu.co/es-co/Paginas/default.aspx", urlTitle: "", img: "SENA.jpeg", text: "Planeación Estratégica de Proyectos // Servicio Nacional de Aprendizaje (SENA)." },
+    { id: 5, fecha: "2011", url: "https://www.sena.edu.co/es-co/Paginas/default.aspx", urlTitle: "", img: "SENA.jpeg", text: "Diseño de Instrumentos de Evaluación // Servicio Nacional de Aprendizaje (SENA)." },
+    { id: 6, fecha: "2012 - 2013", url: "https://www.sena.edu.co/es-co/Paginas/default.aspx", urlTitle: "", img: "/SENA.jpeg", text: "Inglés (Curso completo) Niveles I al V // Servicio Nacional de Aprendizaje (SENA).." },
+    { id: 7, fecha: "2015", url: "https://learndigital.withgoogle.com/activate/courses", urlTitle: "", img: "GOOGLE-ACT.png", text: "Introducción al Desarrollo Web // Google Activate." },
+    { id: 8, fecha: "2015", url: "https://www.msjc.edu/", urlTitle: "", img: "MSJC.png", text: "Fundamental English Writing // Mt. San Jacinto College." },
+    { id: 9, fecha: "2017", url: "https://iei.ua.es/", urlTitle: "", img: "IEI-ALICANTE.jpg", text: "Curso de Desarrollo Web // Instituto de Economía Internacional de la Universidad de Alicante." },
+    { id: 10, fecha: "2022 - 2022", url: "https://yoprogramo.org.ar/modulos-fase-1/", urlTitle: "", img: "SeProg.png", text: "#SéProgramar // Argentina Programa , INTI." },
+    { id: 11, fecha: "2022 - Actual", url: "https://www.yoprogramo.org.ar/", urlTitle: "", img: "YoProg.png", text: "#YoProgramo // Argentina Programa , INTI." },
   ]
 }
 
--- src/app/componentes/experiencia/experiencia.component.ts
+++ src/app/componentes/experiencia/experiencia.component.ts
@@ -14,15 +14,15 @@ export class ExperienciaComponent implements OnInit {
   constructor() { }
   ngOnInit() {
     this.experience = [
-      { id: 0, fecha: "2010 - 2022", url: "https://www.ujmv.edu/", urlTitle: "", img: "assets/images/UJMV.png", text: "Profesor de aula virtual en la Universidad José María Vargas." },
-      { id: 1, fecha: "2020 - 2023", url: "https://www.youtube.com/@UniVistaTV/about/", urlTitle: "", img: "src/assets/images/Logo UniVista.jpeg", text: "Redactor y curador en Univista TV." },
-      { id: 2, fecha: "2013 - 2016", url: "https://www.isum.com.ve", urlTitle: "", img: "assets/images/ISUM.png", text: "Profesor de Redacción en el Instituto de Mercadotecnia.." },
-      { id: 3, fecha: "2009 - 2016", url: "https://www.instagram.com/unihumboldt/?hl=en", urlTitle: "", img: "images/UAH.png", text: "Profesor de Redacción en la Universidad Alejandro de Humboldt.." },
-      { id: 4, fecha: "2006 - 2016", url: "https://usm.edu.ve/", urlTitle: "", img: "images/USM.png", text: "Profesor de redacción en la Universidad Santa María.." },
-      { id: 5, fecha: "2003 - 2006", url: "https://www.instagram.com/una.edu.ve/", urlTitle: "", img: "images/UNA.jpeg", text: "Redactor de contenidos en la Universidad Nacional Abierta." },
-      { id: 6, fecha: "2000 - 2002", url: "https://www.cavecol.org/", urlTitle: "", img: "images/logo_cavecol.png", text: "Redactor de contenidos en CAVECOL." },
-      { id: 7, fecha: "1995 - 1999", url: "https://www.calientefm.com.ve", urlTitle: "", img: "images/Caliente Stereo 105.9 FM.png", text: "Redactor de noticias en la emisora de radio Caliente Estéreo FM 105.9." },
-      { id: 8, fecha: "1989 - 2000", url: "https://salesianosvenezuela.com", urlTitle: "", img: "images/CAS.png", text: "Redactor de contenidos en el Centro Audivisual Salesiano.." },
+      { id: 0, fecha: "2010 - 2022", url: "https://www.ujmv.edu/", urlTitle: "", img: "UJMV.png", text: "Profesor de aula virtual en la Universidad José María Vargas." },
+      { id: 1, fecha: "2020 - 2023", url: "https://www.youtube.com/@UniVistaTV/about/", urlTitle: "", img: "Logo UniVista.jpeg", text: "Redactor y curador en Univista TV." },
+      { id: 2, fecha: "2013 - 2016", url: "https://www.isum.com.ve", urlTitle: "", img: "ISUM.png", text: "Profesor de Redacción en el Instituto de Mercadotecnia.." },
+      { id: 3, fecha: "2009 - 2016", url: "https://www.instagram.com/unihumboldt/?hl=en", urlTitle: "", img: "UAH.png", text: "Profesor de Redacción en la Universidad Alejandro de Humboldt.." },
+      { id: 4, fecha: "2006 - 2016", url: "https://usm.edu.ve/", urlTitle: "", img: "USM.png", text: "Profesor de redacción en la Universidad Santa María.." },
+      { id: 5, fecha: "2003 - 2006", url: "https://www.instagram.com/una.edu.ve/", urlTitle: "", img: "UNA.jpeg", text: "Redactor de contenidos en la Universidad Nacional Abierta." },
+      { id: 6, fecha: "2000 - 2002", url: "https://www.cavecol.org/", urlTitle: "", img: "logo_cavecol.png", text: "Redactor de contenidos en CAVECOL." },
+      { id: 7, fecha: "1995 - 1999", url: "https://www.calientefm.com.ve", urlTitle: "", img: "Caliente Stereo 105.9 FM.png", text: "Redactor de noticias en la emisora de radio Caliente Estéreo FM 105.9." },
+      { id: 8, fecha: "1989 - 2000", url: "https://salesianosvenezuela.com", urlTitle: "", img: "CAS.png", text: "Redactor de contenidos en el Centro Audivisual Salesiano.." },
     ]
   }
 
